a0960ad81031f3f524367513ec9000248d6084be
lawrencematthew19942016 <80303704+lawrencematthew19942016@users.noreply.github.com>
2021-10-21 16:53:22 +1100

    Update DrugBlock.sol

--- DrugBlock.sol
+++ DrugBlock.sol
@@ -1,53 +1,105 @@
 pragma solidity ^0.8.6;
 
 
+
 contract DrugBlockTest{
     struct Drug {
         
         uint drugId; 
         string drugName; 
         string brandName;
-        string ExpiryDate;
-        //whole numbers only
-        uint UnitPrice;
-        uint SalesPrice;
+        uint Dday;
+        uint Dmonth;
+        uint Dyear; 
+        uint eDay;
+        uint eMonth;
+        uint eYear; 
+        uint drugCount;
         bool DrugDelivered; 
-        
-        //uint TrackingID;
-        
+   
     }
     
     Drug[] public drugs; 
     uint public nextId;
     uint public trackingID; 
 
-    
-    function create(string memory drugName, string memory brandName, string memory ExpiryDate, uint UnitPrice, uint SalesPrice, bool DrugDelivered) public {
-        drugs.push(Drug(nextId, drugName, brandName, ExpiryDate, UnitPrice, SalesPrice, DrugDelivered)); 
-        nextId++; 
+
+    function create(string memory drugName, string memory brandName, 
+    uint Dday, uint Dmonth, uint Dyear, 
+    uint eDay, uint eMonth, uint eYear, uint drugCount,
+    bool DrugDelivered) public {
+        
+        
+        if(drugCount <= 3){
+            revert('Drug stock too low, please restock');
+        }
+       
         if(DrugDelivered == false)
         { 
             revert("Error");
         }
-    }
+        //alternative to the error function 
+        if(Dyear == eYear) {
+            if(Dmonth == eMonth)
+            if(Dday >= eDay){
+                revert("This drug has expired, please discard");
+            }
+            
+         if(Dyear >= eYear) {
+            if(Dmonth == eMonth)
+            if(Dday >= eDay){
+                revert("This drug has expired, please discard");
+            }
+            
+        }
+        
+        if(Dyear >= eYear) {
+            if(Dmonth >= eMonth)
+            if(Dday >= eDay){
+                revert("This drug has expired, please discard");
+            }
+            
+        }
+    } else {
+        
+    drugs.push(Drug(nextId, drugName, brandName, Dday, Dmonth, Dyear, eDay, eMonth, eYear, drugCount,DrugDelivered)); 
+        nextId++;
+       
+   }
+   }  
     
-    function read(uint drugId) view public returns(uint, string memory, string memory, string memory ExpiryDate, uint UnitPrice, uint SalesPrice, bool DrugDelivered)
+    // function expiryDrug() public{
+        
+    // }
+    
+    function read(uint drugId) view public returns(uint, string memory, string memory, 
+    uint Dday, uint Dmonth, uint Dyear, 
+    uint eDay, uint eMonth, uint eYear, uint drugCount,
+   bool DrugDelivered)
     {
         uint i = find(drugId);
        
-            return(drugs[i].drugId, drugs[i].drugName, drugs[i].brandName, drugs[i].ExpiryDate, drugs[i].UnitPrice, drugs[i].SalesPrice, drugs[i].DrugDelivered); 
+            return(drugs[i].drugId, drugs[i].drugName, drugs[i].brandName, drugs[i].Dday, drugs[i].Dmonth, drugs[i].Dyear, drugs[i].eDay, drugs[i].eMonth, drugs[i].eYear,
+             drugs[i].drugCount, drugs[i].DrugDelivered); 
       
     }
     
     //to-do need updateBrand
-    function update(uint drugId, string memory drugName, string memory brandName, string memory ExpiryDate, uint UnitPrice, uint SalesPrice, bool DrugDelivered) public
+    function update(uint drugId, string memory drugName, string memory brandName, 
+    uint Dday, uint Dmonth, uint Dyear, 
+    uint eDay, uint eMonth, uint eYear,uint drugCount,
+  bool DrugDelivered) public
     {
         uint i = find(drugId);
              drugs[i].drugName = drugName; 
              drugs[i].brandName = brandName; 
-             drugs[i].ExpiryDate = ExpiryDate;
-             drugs[i].UnitPrice = UnitPrice;
-             drugs[i].SalesPrice = SalesPrice;
+             drugs[i].Dday = Dday; 
+             drugs[i].Dmonth = Dmonth; 
+             drugs[i].Dyear = Dyear; 
+             drugs[i].eDay = eDay; 
+             drugs[i].eMonth = eMonth; 
+             drugs[i].eYear = eYear; 
+             drugs[i].drugName = drugName;
              drugs[i].DrugDelivered = DrugDelivered;
          
      
@@ -77,21 +129,4 @@ contract DrugBlockTest{
 }
 
 
-// contract Pharmacy{
-    
-//     struct PharmDrug
-    
-//     {
-//         uint drugId; 
-//         string drugName; 
-//         string brandName;
-//         string ExpiryDate;
-//         //whole numbers only
-//         uint UnitPrice;
-//         uint SalesPrice;
-//     }
-    
-    
-    
-    
-// }
+
